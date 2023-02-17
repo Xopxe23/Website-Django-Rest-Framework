411942d7e10a8df7b864d72bd0800522968cc4bc
Xopxe23 <georgeduday@gmail.com>
2023-02-17 13:30:31 +0300

    Введение в сериализацию

--- women/serializers.py
+++ women/serializers.py
@@ -1,8 +1,38 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+
 from .models import Women
+from rest_framework.renderers import JSONRenderer
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = WomenModel('Анджелина Джоли', 'Контент Анджелины Джоли')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title": "Angelina Jolie", "content": "Content: Angelina Jolie"}')
+#     data = JSONParser().parse(stream=stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
--- women/views.py
+++ women/views.py
@@ -10,16 +10,19 @@ from .serializers import WomenSerializer
 
 class WomenApiView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = Women.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
-        return Response({'post': model_to_dict(instance=post_new)})
+        return Response({'post': WomenSerializer(post_new).data})
 
 
 # class WomenApiView(generics.ListAPIView):
