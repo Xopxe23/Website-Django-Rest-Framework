c32ae0c3d5cf66ee291d9636db5a460515f71ecd
Xopxe23 <georgeduday@gmail.com>
2023-02-17 10:07:25 +0300

    Базовый класс APIView

--- women/views.py
+++ women/views.py
@@ -1,9 +1,27 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenApiView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenApiView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(instance=post_new)})
+
+
+# class WomenApiView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
