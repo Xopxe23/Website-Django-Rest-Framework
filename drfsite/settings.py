d0c4d9c03f010098ec5e456f34cdb1bd9a761819
Xopxe23 <georgeduday@gmail.com>
2023-02-19 11:49:01 +0300

    Добавляем пагинацию(Pagination)

--- drfsite/settings.py
+++ drfsite/settings.py
@@ -137,5 +137,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
 }
--- women/views.py
+++ women/views.py
@@ -2,6 +2,7 @@ from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,6 +12,32 @@ from .models import Women, Category
 from .serializers import WomenSerializer
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
+
+
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = WomenAPIListPagination
+
+
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticated, )
+    # authentication_classes = (TokenAuthentication,)
+
+
+class WomenAPIDestroyView(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly, )
 
 
 # class WomenViewSet(mixins.CreateModelMixin,
@@ -25,25 +52,6 @@ from rest_framework.decorators import action
 #     def category(self, request):
 #         cats = Category.objects.all()
 #         return Response({'cats': [c.name for c in cats]})
-
-
-class WomenAPIList(generics.ListCreateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
-
-
-class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-    permission_classes = (IsAuthenticated, )
-    # authentication_classes = (TokenAuthentication,)
-
-
-class WomenAPIDestroyView(generics.RetrieveDestroyAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-    permission_classes = (IsAdminOrReadOnly, )
 
 
 # class WomenApiView(APIView):
